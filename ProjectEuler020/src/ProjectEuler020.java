44cd747176fc7804b75e006642af9cfc37859c66
Mike Overby <MikeOverby@outlook.com>
2014-09-18 21:08:33 -0700

    Problem Solved

--- ProjectEuler020/src/ProjectEuler020.java
+++ ProjectEuler020/src/ProjectEuler020.java
@@ -1,4 +1,30 @@
+import java.math.BigInteger;
 
 public class ProjectEuler020 {
-
+		
+	public static int run()
+	{
+		//Multiply numbers from  1 to 100
+		BigInteger num = new BigInteger("1");
+		for (int i = 1; i <= 100; i++)
+		{
+				num = num.multiply(BigInteger.valueOf((i)));
+		}
+		
+		//Convert the result to a string
+		String numberString = num.toString();
+		
+		//Step through the string, summing each digit.
+		int sum = 0;
+		for (int i = 0; i < numberString.length(); i++)
+			sum += Integer.decode(""+numberString.charAt(i));
+		
+		//Return the sum. That's the answer.
+		return sum;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.print(ProjectEuler020.run());
+	}
 }
