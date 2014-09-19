03a2322d1ffe55f19f7371594626a92e2fcf0964
Mike Overby <MikeOverby@outlook.com>
2014-09-19 15:36:27 -0700

    Attempt to solve the problem.

--- ProjectEuler206.java
+++ ProjectEuler206.java
@@ -5,12 +5,22 @@ public class ProjectEuler206 {
 	{
 		//The integer must end in 0 because its square ends in 0.
 		//The Square has 19 digits
-			//The integer may have 9 digits
-				//300_000_000 squared has 18 digits.
-				//999_999_999 squared has 19 digits.
-		for (int loopCount = 300_000_000; loopCount < 999_999_999; loopCount++)
+			//The integer may have 10 digits
+				//317_000_000 squared has 19 digits. The Square starts with 1.
+				//499_999_999 squared has 19 digits. The Square starts with 2.
+		for (long loopCount = 3_170_000_000; loopCount < 4_999_999_999; loopCount+=10)
 		{
-			
+			number = loopCount * loopCount;
+			if (number / 10e18 == 1)
+				if ((number / 10e16) % 10 == 2)
+					if ((number / 10e14) % 10 == 3)
+						if ((number / 10e12) % 10 == 4)
+							if ((number / 10e10) % 10 == 5)
+								if ((number / 10e8) % 10 == 6)
+									if ((number / 10e6) % 10 == 7)
+										if ((number / 10e4) % 10 == 8)
+											if ((number / 10e2) % 10 == 9)
+												return number;
 		}
 	}
 	
