2aed0f0c1f1742c82b33d55f9f38479f384d87c5
Mike Overby <MikeOverby@outlook.com>
2014-09-19 16:08:02 -0700

    Edits. I'm struggling to get the java files from git to run in Eclipse.

--- ProjectEuler206.java
+++ ProjectEuler206.java
@@ -1,16 +1,16 @@
 //Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 //where each “_” is a single digit.
 public class ProjectEuler206 {
-	public static int run()
+	public static long run()
 	{
 		//The integer must end in 0 because its square ends in 0.
 		//The Square has 19 digits
 			//The integer may have 10 digits
 				//317_000_000 squared has 19 digits. The Square starts with 1.
 				//499_999_999 squared has 19 digits. The Square starts with 2.
-		for (long loopCount = 3_170_000_000; loopCount < 4_999_999_999; loopCount+=10)
+		for (long loopCount = 3_170_000_000L; loopCount < 4_999_999_999L; loopCount+=10)
 		{
-			number = loopCount * loopCount;
+			long number = loopCount * loopCount;
 			if (number / 10e18 == 1)
 				if ((number / 10e16) % 10 == 2)
 					if ((number / 10e14) % 10 == 3)
@@ -22,11 +22,11 @@ public class ProjectEuler206 {
 											if ((number / 10e2) % 10 == 9)
 												return number;
 		}
+		return 1;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(ProjectEuler206.run());
 
 	}
-
 }
