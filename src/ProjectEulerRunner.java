fe7229086e16b0f99081b7fdd2f2e578c4f1b1cf
Mike Overby <MikeOverby@outlook.com>
2014-09-21 11:49:02 -0700

    Transferring all files into a src folder
    I'd prefer to do this a different way, honestly. I'm going to delete the
    original files, and these new files will not be connected to their old
    commits. Eclipse, why can't you just work?

---
+++ src/ProjectEulerRunner.java
@@ -0,0 +1,11 @@
+
+public class ProjectEulerRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.print(ProjectEuler015.run());
+		System.out.print(ProjectEuler020.run());
+		System.out.print(ProjectEuler030.run());
+		System.out.print(ProjectEuler206.run());
+	}
+}
