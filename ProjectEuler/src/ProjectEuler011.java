d4626922a7921e231fed7a26ce64865ece57befd
Mike Overby <MikeOverby@outlook.com>
2014-10-03 20:10:26 -0700

    Solved Problem 011.

--- ProjectEuler/src/ProjectEuler011.java
+++ ProjectEuler/src/ProjectEuler011.java
@@ -2,65 +2,106 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Scanner;
 
-public final class ProjectEuler011 {
+public final class ProjectEuler011
+{
 
-	/**
-	 * The structure to hold and access the grid values.
-	 */
-	private static int[][] grid;
-	
-	/**
-	 * Size of the square grid
-	 */
-	private final static int GRID_SIZE = 20;
-	
-	/**
-	 * Stores the largest product of four adjacent numbers in the grid.
-	 */
-	private static int maxProduct = 0;
-	
-	private static void fillGrid() throws FileNotFoundException
-	{
-		Scanner in = new Scanner(new File("files/011_input"));
-		
-		int index = 0;
-		while(in.hasNextLine())
-		{
-			String[] temp = in.nextLine().split(" ");
-			for (int j = 0; j < temp.length; j++)
-				grid[index][j] = Integer.decode(temp[j]);
-		}
-	}
-	public static long run() throws FileNotFoundException
-	{
-		//Fill grid
-		grid = new int[GRID_SIZE][GRID_SIZE];
-		fillGrid();
-		
-		//Solve all horizontal
-		for (int row = 0; row < grid.length; row++)
-			for (int col = 3; col < grid[row].length; col++)
-			{
-				int product = grid[row][col-3] * grid[row][col-2] * grid[row][col-1] * grid[row][col];
-				if (product > maxProduct)
-					maxProduct = product;
-			}
-		
-		//Solve all vertical
-		for (int col = 0; col < grid.length; col++)
-			for (int row = 3; row < grid[row].length; row++)
-			{
-				int product = grid[row-3][col] * grid[row-2][col] * grid[row-1][col] * grid[row][col];
-				if (product > maxProduct)
-					maxProduct = product;
-			}
-		
-		//Solve all left diagonals
-		
-		//Solve all right diagonals
-		
-		return maxProduct;
-	}
+    /**
+     * The structure to hold and access the grid values.
+     */
+    private static int[][] grid;
+
+    /**
+     * Size of the square grid
+     */
+    private final static int GRID_SIZE = 20;
+
+    /**
+     * Stores the largest product of four adjacent numbers in the grid.
+     */
+    private static int maxProduct = 0;
+
+    private static void fillGrid()
+    {
+        try
+        {
+            File file = new File("src/011_Input");
+            Scanner in = new Scanner(file);
+
+            int index = 0;
+            while (in.hasNextLine())
+            {
+                String[] temp = in.nextLine().split(" "); // Get next row from file
+                for (int j = 0; j < temp.length; j++) // Fill the next row in grid
+                {
+                    if (temp[j].substring(0, 1).equals("0")) // If there is a leading zero, that makes Integer.decode consider it as an octal
+                        grid[index][j] = Integer.decode(temp[j].substring(1)); // Skip leading zero. The numbers are all under 100, so there is no "001" numbers.
+                    else
+                        grid[index][j] = Integer.decode(temp[j]); // If there is no leading zero, just take the whole number
+                }
+                
+                index++; // Next row
+            }
+
+            in.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            // System.out.print("Problem 011 File Not Found ");
+            // e.printStackTrace();
+        }
+    }
+
+    public static long run()
+    {
+        // Fill grid
+        // row col
+        grid = new int[GRID_SIZE][GRID_SIZE];
+        fillGrid();
+
+        // Solve all horizontal
+        for (int row = 0; row < grid.length; row++)
+            for (int col = 3; col < grid.length; col++)
+            {
+                int product = grid[row][col - 3] * grid[row][col - 2] * grid[row][col - 1] * grid[row][col];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+
+        // Solve all vertical
+        for (int col = 0; col < grid.length; col++)
+        {
+            for (int row = 3; row < grid.length; row++)
+            {
+                int product = grid[row - 3][col] * grid[row - 2][col] * grid[row - 1][col] * grid[row][col];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+        }
+
+        // Solve all left diagonals
+        for (int col = grid.length-4; col > 0; col--)
+        {
+            for (int row = 3; row < grid.length; row++)
+            {
+                int product = grid[row][col] * grid[row - 1][col + 1] * grid[row - 2][col + 2] * grid[row - 3][col + 3];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+        }
+        // Solve all right diagonals
+        for (int col = 3; col < grid.length; col++)
+        {
+            for (int row = 3; row < grid.length; row++)
+            {
+                int product = grid[row][col] * grid[row - 1][col - 1] * grid[row - 2][col - 2] * grid[row - 3][col - 3];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+        }
+        
+        return maxProduct;
+    }
 }
--- ProjectEuler/src/ProjectEulerRunner.java
+++ ProjectEuler/src/ProjectEulerRunner.java
@@ -3,87 +3,80 @@ import java.util.Scanner;
 
 public class ProjectEulerRunner
 {
-	public static void main(String[] args)
-	{
-		Scanner in = new Scanner(System.in);
-		
-		//TODO: Check if what's entered is actually an int
-		System.out.println("Enter Problem Number: ");
-		int choice = in.nextInt();
-		in.close();
-		
-		//TODO: Print approximate solve time.
-		switch(choice)
-		{
-//		case NUM:
-//		System.out.println("Solve Time: Unrecorded");
-//		System.out.println(ProjectEulerNUM.run() + " (Answer: Unsolved)");
-//		break;
-		
-			case 8:
-				System.out.println("Solve Time: 96 ms");
-				System.out.println(ProjectEuler008.run() + " (Answer: 23514624000)");
-				break;
-		
-			case 10:
-				System.out.println("Solve Time: 73");
-				System.out.println(ProjectEuler010.run() + " (Answer: 142913828922)");
-				break;
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
 
-			case 11:
-				System.out.println("Solve Time: Unrecorded");
-				try
-				{
-					System.out.println(ProjectEuler011.run() + " (Answer: Unsolved)");
-				}
-				catch(FileNotFoundException e)
-				{
-					System.out.print("Problem 011 File Not Found."); System.exit(1);
-				}
-				break;
-				
-			case 15:
-				System.out.println("Solve Time: 0 ms");
-				System.out.println(ProjectEuler015.run() + " (Answer: 137846528820)");
-				break;
-				
-			case 16:
-				System.out.println("Solve Time: Unrecorded.");
-				System.out.println(ProjectEuler016.run() + " (Answer: 1366)");
-				break;
-				
-			case 20:
-				System.out.println("Solve Time: Unrecorded.");
-				System.out.println(ProjectEuler020.run() + " (Answer: 648)");
-				break;
-				
-			case 30:
-				System.out.println("Solve Time: Unrecorded.");
-				System.out.println(ProjectEuler030.run() + " (Answer: 443839)");
-				break;
-			
-			case 32:
-				System.out.println("Solve Time: 368");
-				System.out.println(ProjectEuler032.run() + " (Answer: 45228)");
-				break;
-				
-			case 92:
-				System.out.println("Solve Time: Unrecorded.");
-				System.out.println(ProjectEuler092.run() + " (Answer: 8581146)");
-				break;	
+        // TODO: Check if what's entered is actually an int
+        System.out.println("Enter Problem Number: ");
+        int choice = in.nextInt();
+        in.close();
 
-			case 125:
-				System.out.println("Solve Time: 1027323");
-				System.out.println(ProjectEuler125.run() + " (Answer: 2906969179)");
-				break;
-				
-			case 206:
-				System.out.println("Solve Time: Unrecorded.");
-				System.out.println(ProjectEuler206.run() + " (Answer: 1389019170)");
-				break;
-				
-			default:
-				System.out.println("That problem is unsolved.");
-		}
-	}
+        // TODO: Print approximate solve time.
+        switch (choice)
+        {
+            // case NUM:
+            // System.out.println("Solve Time: Unrecorded");
+            // System.out.println(ProjectEulerNUM.run() + " (Answer: Unsolved)");
+            // break;
+
+            case 8:
+                System.out.println("Solve Time: 96 ms");
+                System.out.println(ProjectEuler008.run() + " (Answer: 23514624000)");
+                break;
+    
+            case 10:
+                System.out.println("Solve Time: 73 ms");
+                System.out.println(ProjectEuler010.run() + " (Answer: 142913828922)");
+                break;
+    
+            case 11:
+                System.out.println("Solve Time: 47 ms");
+                System.out.println(ProjectEuler011.run() + " (Answer: 70600674)");
+                break;
+    
+            case 15:
+                System.out.println("Solve Time: 0 ms");
+                System.out.println(ProjectEuler015.run() + " (Answer: 137846528820)");
+                break;
+    
+            case 16:
+                System.out.println("Solve Time: Unrecorded.");
+                System.out.println(ProjectEuler016.run() + " (Answer: 1366)");
+                break;
+    
+            case 20:
+                System.out.println("Solve Time: Unrecorded.");
+                System.out.println(ProjectEuler020.run() + " (Answer: 648)");
+                break;
+    
+            case 30:
+                System.out.println("Solve Time: Unrecorded.");
+                System.out.println(ProjectEuler030.run() + " (Answer: 443839)");
+                break;
+    
+            case 32:
+                System.out.println("Solve Time: 368 ms");
+                System.out.println(ProjectEuler032.run() + " (Answer: 45228)");
+                break;
+    
+            case 92:
+                System.out.println("Solve Time: Unrecorded.");
+                System.out.println(ProjectEuler092.run() + " (Answer: 8581146)");
+                break;
+    
+            case 125:
+                System.out.println("Solve Time: 1027323 ms");
+                System.out.println(ProjectEuler125.run() + " (Answer: 2906969179)");
+                break;
+    
+            case 206:
+                System.out.println("Solve Time: Unrecorded.");
+                System.out.println(ProjectEuler206.run() + " (Answer: 1389019170)");
+                break;
+    
+            default:
+                System.out.println("That problem is unsolved.");
+        }
+    }
 }
